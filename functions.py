c6e4562548d695de26c3872b75b7cdb42da6a333
Maxime Ellerbach <maxim@Laurent-PC.(none)>
2020-01-22 15:42:41 +0100

    better image segmentation

--- functions.py
+++ functions.py
@@ -6,18 +6,18 @@ def create_cap(c = 0):
     cap = cv2.VideoCapture(c)
     return cap
 
-def capture_img(cap):
+def capture_img(cap, size):
     _, img = cap.read()
+    h, w, ch = img.shape
+    img = cv2.resize(img, (int(w*size[0]), int(h*size[1])))
+    v = np.median(img)
     img = close_img(img, 255)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.bilateralFilter(img, 9, 35, 35)
+    blurred = cv2.bilateralFilter(img, 5, 70, 70)
 
-    return img, gray, blurred
+    return img, gray, blurred, v
 
-def auto_canny(image, sigma=0.33):
-	# compute the median of the single channel pixel intensities
-	v = np.median(image)
- 
+def auto_canny(image, v, sigma=0.33):
 	# apply automatic Canny edge detection using the computed median
 	lower = int(max(0, (1.0 - sigma) * v))
 	upper = int(min(255, (1.0 + sigma) * v))
@@ -26,10 +26,9 @@ def auto_canny(image, sigma=0.33):
 	# return the edged image
 	return edged
 
-def detect_edges(img):
-    edge = auto_canny(img)
-    # edge = average_edges(edge)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ERODE, (5,5))
+def detect_edges(img, v):
+    edge = auto_canny(img, v, sigma=0.5)
+    kernel = cv2.getStructuringElement(cv2.MORPH_DILATE, (3,3))
     dilated = cv2.dilate(edge, kernel)
     return cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -37,8 +36,8 @@ def get_mask(cnts, hierarchy, img):
     mask = np.zeros(img.shape, np.uint8)
     for it, cnt in enumerate(cnts):
         hcnt = hierarchy[it][-1]
-        if hcnt != -1 and hcnt<=1:
-            b, g, r = np.random.randint(0, 255, 3, dtype=np.uint8)
+        if -1<hcnt: #  and cv2.contourArea(cnt)>150
+            b, g, r = np.random.randint(0, 127, 3, dtype=np.uint8)
             cv2.drawContours(mask, [cnt], 0, (int(b), int(g), int(r)), -1)
             cv2.drawContours(img, [cnt], 0, (0, 255, 0), 1)
 
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from functions import *
+from glob import glob
 
-cap = create_cap(0)
+cap = create_cap(0) # "F:\\video-fh4\\MFAwpTjlSY_Trim.mp4" FtcBrYpjnA_Trim
+resize_fact= (1, 1)
 
 while(1):
-    img, gray, blurred = capture_img(cap)
+    img, gray, blurred, v = capture_img(cap, resize_fact)
 
-    _, cnts, hierarchy = detect_edges(blurred)
+    im2, cnts, hierarchy = detect_edges(blurred, v)
     h, w, ch = img.shape
 
     masked, img = get_mask(cnts, hierarchy[0], img)
 
     cv2.imshow('img', img)
-    cv2.imshow('gray', gray)
+    cv2.imshow('gray', im2)
     cv2.imshow('blurred', blurred)
     cv2.imshow('masked', masked)
 
